0698d1fe234b6610965c89f637beae84ab602ced
Renan Fernandes Silva <renan.silva@audaces.com>
2022-07-28 20:11:44 -0300

    add models folder and identation

---
+++ WordCounter/Models/UserForm.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WordCounter.Models
+{
+    public class UserForm
+    {
+        [Required(ErrorMessage = "Some text input is required.")]
+        public string? InsertedText { get; set; }
+    }
+}
